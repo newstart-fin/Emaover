97e221dc834dcc45ab24c275633ba991dd36c001
newstart-fin <bachhaaditya6@gmail.com>
2025-07-18 22:47:36 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+
+def check_ema_crossover():
+    print("Checking EMA crossover...")
+    # Your trading logic here
+
+while True:
+    check_ema_crossover()
+    time.sleep(60)  # every 1 minute
